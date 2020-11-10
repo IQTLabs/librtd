6c6590399523edfe321a32bd5bc2765a9253c3bb
Benjamin Lee <benjamindlee@me.com>
2020-11-09 21:21:32 -0500

    Remove function exports to Python that were deleted

--- librtd-py/librtdpy/librtdpy.nim
+++ librtd-py/librtdpy/librtdpy.nim
@@ -2,14 +2,8 @@ import librtd
 import tables
 import nimpy
 
-func kmer_indices*(x: string, k: Positive): Table[string, seq[int]] {.exportpy.} =
-  librtd.kmerIndices(x, k)
-
 func same_kmer_return_times*(x: string, k: Positive): Table[string, seq[int]] {.exportpy.} =
   librtd.sameKmerReturnTimes(x, k)
-
-func dist_to_next_greater_index*(indicies1: seq[int], indices2: seq[int]): seq[int] {.exportpy.} =
-  librtd.distToNextGreaterIndex(indicies1, indices2)
 
 func pairwise_kmer_return_times*(x: string, k: Positive): Table[string, seq[int]] {.exportpy.} =
   librtd.pairwiseKmerReturnTimes(x, k)
